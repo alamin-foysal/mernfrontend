0e8a3a43f768e66311bee73ac593e316c5e454fb
unknown <foysalk877@gmail.com>
2023-10-03 21:30:34 +0600

    loading add in app js

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,28 @@ import Product from "./pages/Product";
 import Deshbord from "./pages/Deshbord";
 import Topdesh from "./Components/Top_pro_deshbord/Topdesh";
 import Sliderdesh from "./Components/Slider_deshbord/Sliderdesh";
+import { useState } from "react";
 
 function App() {
+  const[loading,setLoading]=useState(true)
+  setTimeout(()=>{
+    setLoading(false)
+  },4000)
   return (
     <>
-      <div>
-        <Routes>
-          <Route path="/" element={<Myhome />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/deshbord" element={<Deshbord />}>
-            <Route path="topdesh" element={<Topdesh/>} />
-            <Route path="sliderdesh" element={<Sliderdesh/>} />
-          </Route>
-        </Routes>
-      </div>
+    {
+      loading?<div><h1 className="text-center mt-5">please wait a few mint....</h1></div>:<div>
+      <Routes>
+        <Route path="/" element={<Myhome />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/deshbord" element={<Deshbord />}>
+          <Route path="topdesh" element={<Topdesh/>} />
+          <Route path="sliderdesh" element={<Sliderdesh/>} />
+        </Route>
+      </Routes>
+    </div>
+    }
+      
     </>
   );
 }
